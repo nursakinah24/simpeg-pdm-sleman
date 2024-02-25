ef72ac1c90d658b6e2b49775774df9757eea8ecc
Nur Sakinah <nursakinah2402@gmail.com>
2024-02-26 01:16:47 +0700

    feat: Add buttons

---
+++ src/components/Buttons/deleteButton.vue
@@ -0,0 +1,3 @@
+<template>
+     <button class="py-2 px-4 w-[4.5rem] bg-red-500 hover:bg-red-600 focus:ring-red-400 focus:ring-offset-red-300 text-white transition ease-in duration-200 text-center text-xs font-semibold font-poppins shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg">Hapus</button>
+</template>
---
+++ src/components/Buttons/downloadButton.vue
@@ -0,0 +1,3 @@
+<template>
+    <button class="py-2 px-4 w-[4.5rem] bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-400 text-white transition ease-in duration-200 text-center text-xs font-semibold font-poppins shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg">Unduh</button>
+</template>
