cb516d01c2027b4d704bdc0d7654f071fcc54eca
Nur Sakinah <nursakinah2402@gmail.com>
2024-02-25 17:05:54 +0700

    feat: Add footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="relative w-screen bg-transparent p-4 flex justify-center items-center h-full">
+    <span class="text-center text-xs font-poppins text-white/75">
+      Sistem Informasi Kepegawaian PDM Sleman Developed by Program Studi Teknologi Informasi UMY ©2024
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
