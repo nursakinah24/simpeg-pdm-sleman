3e401b758c5b5599e2464fb8f3dd706ec7978dbf
Nur Sakinah <nursakinah2402@gmail.com>
2024-02-26 21:14:51 +0700

    feat: Add dashboard

--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -2,24 +2,23 @@
     <div class="pt-8 px-10 lg:px-20 w-full relative xl:px-20 text-gray-900">
       <div class="pb-6">
         <h1 class="font-poppins font-bold text-3xl">
-          Selamat Datang, Diva Nafisyah
+          Assalamualaikum, Diva Nafisyah
         </h1>
       </div>
-
-      <div class="flex flex-col gap-10 md:flex-row items-center p-8 bg-white shadow rounded-lg mb-4">
-        <div class="w-96 md:1/3 flex justify-center">
-            <img class="rounded-full h-48 w-48 overflow-hidden shadow-md" src="../assets/profile.png" />
-        </div>
-        <div class="w-full md:w-1/2 flex-1 flex-cols gap-4 font-poppins">
-          <div v-for="(data, index) in datas" :key="index" class="flex items-center mb-4">
-            <div class=" flex w-1/4 md:w-2/3 items-center">
-              <span class="font-bold">{{ data.label }}</span>
-            </div>
-            <div class=" flex items-center">
-              <span>{{ data.value }}</span>
+      <div class="flex flex-row gap-2 p-8 bg-white shadow rounded-lg mb-4">
+          <div class="relative flex-initial p-12 w-80 items-center justify-center">
+              <img class="rounded-full shadow-md h-48 w-48" src="../assets/kucing.jpeg" />
+          </div>
+          <div class="relative py-12 px-4">
+            <div v-for="(data, index) in datas" :key="index" class="flex flex-row items-center space-y-1.5">
+              <div class="relative flex items-center w-56">
+                <span class="font-bold">{{ data.label }}</span>
+              </div>
+              <div class="relative flex items-center w-auto">
+                <span>{{ data.value }}</span>
+              </div>
             </div>
           </div>
-        </div>
       </div>
     </div>
   </template>
@@ -29,14 +28,14 @@
     data() {
       return {
         datas: [
-          { label: 'Nama', value: 'example' },
-          { label: 'Tempat Lahir', value: 'example' },
-          { label: 'Tanggal Lahir', value: 'example' },
-          { label: 'Alamat', value: 'example' },
-          { label: 'No. HP', value: 'example' },
-          { label: 'Kepangkatan Terakhir', value: 'example' },
-          { label: 'Pendidikan Terakhir', value: 'example' },
-          { label: 'Jabatan Terakhir', value: 'example' },
+          { label: 'Nama', value: 'Diva Nafisyah' },
+          { label: 'Tempat Lahir', value: 'Bima' },
+          { label: 'Tanggal Lahir', value: '12 Agustus 2003' },
+          { label: 'Alamat', value: 'Perumahan Griya Ageng Bangunjiwo' },
+          { label: 'No. HP', value: '0853-3869-7868' },
+          { label: 'Kepangkatan Terakhir', value: '-' },
+          { label: 'Pendidikan Terakhir', value: 'SMA' },
+          { label: 'Jabatan Terakhir', value: '-' },
         ],
       };
     },
