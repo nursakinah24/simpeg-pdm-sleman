d7c064da81cf3b8404fad26859dae77b537c8ce1
Nur Sakinah <nursakinah2402@gmail.com>
2024-06-14 10:54:58 +0700

    feat: Add confirm password modal

---
+++ src/components/Alert/confirmPassword.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-if="visible" id="popup-modal"
+        class="fixed inset-0 z-50 overflow-auto bg-gray-500 bg-opacity-50 flex items-center justify-center font-poppins">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow">
+                <div class="p-4 md:p-5 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <h3 class="mb-5 text-lg text-gray-500 ">Keluar untuk ubah password?</h3>
+                    <button @click="confirmLogout" type="submit"
+                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Ya
+                    </button>
+                    <button @click="closeModal" type="button"
+                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 ">Tidak</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+    components: {
+        'font-awesome-icon': FontAwesomeIcon,
+    },
+    props: {
+        visible: Boolean,
+       // id: String
+    },
+    setup(props, { emit }) {
+        const closeModal = () => {
+            emit('close');
+        };
+
+        const confirmLogout = () => {
+            emit('confirm'); // Emit the id along with the confirm event
+            closeModal(); // Close the modal after confirmation
+        };
+
+
+        return { closeModal, confirmLogout };
+    }
+}
+</script>
