92733a20e9bdc0874470ec82b88931563e30e8b1
Nur Sakinah <nursakinah2402@gmail.com>
2024-06-14 10:56:25 +0700

    feat: Add user and admin layout

---
+++ src/layout/AdminLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-gradient-to-r from-blue-col to-green-col">
+    <NavbarAdmin/>
+    <div class="flex">
+      <SidebarAdmin class="" />
+      <div class="flex-1 bg-gray-50 ">
+        <router-view />
+      </div>
+    </div>
+      <Footer />
+  </div>
+  </template>
+  
+  <script>
+  import NavbarAdmin from '../components/Navbar/AdminNavbar.vue';
+  import SidebarAdmin from '../components/Sidebar/AdminSidebar.vue';
+  import Footer from '../components/Footer/Footer.vue';
+  
+  export default {
+    components: {
+      NavbarAdmin,
+      SidebarAdmin,
+      Footer,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add styles for the MainLayout if needed */
+  </style>
+  
---
+++ src/layout/UserLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-gradient-to-r from-green-col to-blue-col">
+    <NavbarUser/>
+    <div class="flex">
+      <SidebarUser class="" />
+      <div class="flex-1 bg-gray-50 ">
+        <router-view />
+      </div>
+    </div>
+      <Footer />
+  </div>
+  </template>
+  
+  <script>
+  import NavbarUser from '../components/Navbar/UserNavbar.vue';
+  import SidebarUser from '../components/Sidebar/UserSidebar.vue';
+  import Footer from '../components/Footer/Footer.vue';
+  
+  export default {
+    components: {
+    NavbarUser,
+    SidebarUser,
+    Footer,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add styles for the MainLayout if needed */
+  </style>
+  
